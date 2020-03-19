dc16daa0f97314c98dc1ccba80fa2469e5a13bba
dinzone <dinamitdana1@gmail.com>
2020-03-19 12:47:02 +0200

    change things

--- utils/TracerRS/index.ts
+++ utils/TracerRS/index.ts
@@ -1,4 +1,4 @@
-import { globalTracer, Span, initGlobalTracer, SpanOptions, followsFrom, Tags as opTags, FORMAT_TEXT_MAP, FORMAT_HTTP_HEADERS, FORMAT_BINARY, SpanContext } from 'opentracing';
+import { globalTracer, Span as opSpan, initGlobalTracer, SpanOptions, followsFrom, Tags as opTags, FORMAT_TEXT_MAP, FORMAT_HTTP_HEADERS, FORMAT_BINARY, SpanContext } from 'opentracing';
 import { initTracer, JaegerTracer, TracingConfig, TracingOptions } from 'jaeger-client';
 import { v1 as uuid } from 'uuid';
 
@@ -11,19 +11,55 @@ export enum ReferenceTypes {
     CHILD_OF = 'child_of',
     FOLLOWS_FROM = 'follow_from'
 }
-class SpanRS extends Span {
-    parentIdRef?: string | null;
-    childrenIdsRef: { [key: string]: boolean } = {};
-    id: string = '';
+class Span {
+    private _span: opSpan;
+    constructor(span: opSpan) {
+        this._span = span;
+    }
+    log(object: { [key: string]: any } | string): opSpan {
+        if (typeof object === 'string') {
+            return this._span.log({
+                message: object
+            });
+        }
+        return this._span.log(object);
+    }
+    error(error: Error, setErrorToTrue: boolean = false): opSpan {
+        if (error instanceof Error) {
+            this.log({
+                'error.name': error.name,
+                'error.message': error.message,
+                'error.stack': error.stack
+            });
+            if (setErrorToTrue) {
+                this.setError(setErrorToTrue);
+            }
+        }
+        return this._span;
+    }
+    tag(object: { [key: string]: any } | string, value?: string | number | boolean): opSpan {
+        if (typeof object === 'string') {
+            return this._span.setTag(object, value);
+        } else {
+            return this._span.addTags(object);
+        }
+    }
+    setError(isError: boolean = true): opSpan {
+        return this.tag({ [opTags.ERROR]: isError });
+    }
+    context(): SpanContext {
+        return this._span.context();
+    }
+    finish(): void {
+        this._span.finish();
+    }
 }
+
 export class Tracer {
     private _tracer!: JaegerTracer;
-    private _activeSpans!: { [key: string]: SpanRS };
-    private _currentActiveSpan?: SpanRS;
     constructor() {
         if (globalTracer()) {
             this._tracer = globalTracer() as JaegerTracer;
-            this._activeSpans = {};
         } else {
             throw new Error('must init tracer before create new instance')
         }
@@ -38,109 +74,85 @@ export class Tracer {
         }, options));
     }
     // start span helper until i get how to extend normally
-    private _startSpan(spanName: string, opt?: SpanOptions): SpanRS {
-        let newSpan = this._tracer.startSpan(spanName, opt) as SpanRS;
-        newSpan.childrenIdsRef = {};
-        newSpan.parentIdRef = null;
+    private _startSpan(spanName: string, opt?: SpanOptions): Span {
+        let newSpan = new Span(this._tracer.startSpan(spanName, opt));
         return newSpan;
     }
-    // start root span
-    private _startOperation(spanName: string): string {
-        const spanId = uuid();
-        const newSpan = this._startSpan(spanName);
-        newSpan.id = spanId;
-        this._currentActiveSpan = newSpan;
-        this._activeSpans[spanId] = newSpan;
-        return spanId;
-    }
-    startSpan(spanName: string, reference: ReferenceTypes = ReferenceTypes.CHILD_OF): string {
-        // in case the root span didnt created
-        if (!this._currentActiveSpan) {
-            return this._startOperation(spanName);
-        }
-        // force ref type of child_of in case this is the second span after the root span
-        if (reference === ReferenceTypes.FOLLOWS_FROM && !this._currentActiveSpan.parentIdRef) {
-            reference = ReferenceTypes.CHILD_OF;
-        }
-        const spanId = uuid();
-        let opt: SpanOptions = {},
-            newSpan: SpanRS;
+    startSpan(spanName: string, span?: Span, reference: ReferenceTypes = ReferenceTypes.CHILD_OF): Span {
+        let opt: SpanOptions = {};
+        if (!span) return this._startSpan(spanName);
         switch (reference) {
-            // default to reference is CHILD_OF
             case ReferenceTypes.CHILD_OF:
-                opt.childOf = this._currentActiveSpan.context();
-                newSpan = this._startSpan(spanName, opt);
-                this._currentActiveSpan.childrenIdsRef[spanId] = true;
-                newSpan.parentIdRef = this._currentActiveSpan.id;
-                this._activeSpans[spanId] = newSpan;
+                opt.childOf = span.context();
                 break;
             case ReferenceTypes.FOLLOWS_FROM:
-                opt.references = [followsFrom(this._currentActiveSpan.context())];
-                newSpan = this._startSpan(spanName, opt);
-                this._activeSpans[this._currentActiveSpan.parentIdRef!].childrenIdsRef[spanId] = true;
-                this._finish(this._currentActiveSpan.id);
-                delete this._activeSpans[this._currentActiveSpan.id];
-                this._currentActiveSpan = newSpan;
+                opt.references = [followsFrom(span.context())];
                 break;
         }
-        return spanId;
+        return this._startSpan(spanName, opt);
     }
-    spanLog(object: { [key: string]: any } | string): void {
-        if (!this._currentActiveSpan) return;
-        if (typeof object === 'string') {
-            this._currentActiveSpan.log({
-                message: object
-            });
-            return;
+    async injectNewSpan(span: Span, func: () => any): Promise<any> {
+        async function _withSpan(this: any) {
+            this.span = span;
+            return await func();
         }
-        this._currentActiveSpan.log(object);
+        return await _withSpan();
     }
-    spanError(error: Error, setErrorToTrue: boolean = false): void {
-        if (error instanceof Error) {
-            this.spanLog({
-                'error.name': error.name,
-                'error.message': error.message,
-                'error.stack': error.stack
-            });
-            if (setErrorToTrue) {
-                this.spanSetError(setErrorToTrue);
-            }
-        }
-    }
-    spanTag(object: { [key: string]: any } | string, value?: string | number | boolean): void {
-        if (!this._currentActiveSpan) return;
-        if (typeof object === 'string') {
-            this._currentActiveSpan.setTag(object, value);
-        } else {
-            this._currentActiveSpan.addTags(object);
-        }
-    }
-    spanSetError(isError: boolean = true): void {
-        this.spanTag({ [opTags.ERROR]: isError });
-    }
-    private _finish(spanId: string) {
-        let currSpan = this._activeSpans[spanId];
-        delete this._activeSpans[spanId];
-        Object.keys(currSpan.childrenIdsRef).forEach(this._finish);
-        currSpan.finish();
-        if (currSpan.parentIdRef && Object.keys(this._activeSpans[currSpan.parentIdRef].childrenIdsRef).length === 0) {
-            this._currentActiveSpan = this._activeSpans[currSpan.parentIdRef];
-            this._finish(currSpan.parentIdRef);
-        } else {
-            this._currentActiveSpan = undefined;
-        }
-    }
-    finish(spanId?: string): void {
-        if (spanId && this._activeSpans[spanId]) {
-            return this._finish(spanId);
-        }
-        if (!this._currentActiveSpan) return;
-        return this._finish(this._currentActiveSpan.id);
-    }
-    inject(format = BaggageFormat.TEXT_MAP): object {
+    // spanLog(object: { [key: string]: any } | string): void {
+    //     if (!this._currentActiveSpan) return;
+    //     if (typeof object === 'string') {
+    //         this._currentActiveSpan.log({
+    //             message: object
+    //         });
+    //         return;
+    //     }
+    //     this._currentActiveSpan.log(object);
+    // }
+    // spanError(error: Error, setErrorToTrue: boolean = false): void {
+    //     if (error instanceof Error) {
+    //         this.spanLog({
+    //             'error.name': error.name,
+    //             'error.message': error.message,
+    //             'error.stack': error.stack
+    //         });
+    //         if (setErrorToTrue) {
+    //             this.spanSetError(setErrorToTrue);
+    //         }
+    //     }
+    // }
+    // spanTag(object: { [key: string]: any } | string, value?: string | number | boolean): void {
+    //     if (!this._currentActiveSpan) return;
+    //     if (typeof object === 'string') {
+    //         this._currentActiveSpan.setTag(object, value);
+    //     } else {
+    //         this._currentActiveSpan.addTags(object);
+    //     }
+    // }
+    // spanSetError(isError: boolean = true): void {
+    //     this.spanTag({ [opTags.ERROR]: isError });
+    // }
+    // private _finish(spanId: string) {
+    //     let currSpan = this._activeSpans[spanId];
+    //     delete this._activeSpans[spanId];
+    //     Object.keys(currSpan.childrenIdsRef).forEach(this._finish);
+    //     currSpan.finish();
+    //     if (currSpan.parentIdRef && Object.keys(this._activeSpans[currSpan.parentIdRef].childrenIdsRef).length === 0) {
+    //         this._currentActiveSpan = this._activeSpans[currSpan.parentIdRef];
+    //         this._finish(currSpan.parentIdRef);
+    //     } else {
+    //         this._currentActiveSpan = undefined;
+    //     }
+    // }
+    // finish(spanId?: string): void {
+    //     if (spanId && this._activeSpans[spanId]) {
+    //         return this._finish(spanId);
+    //     }
+    //     if (!this._currentActiveSpan) return;
+    //     return this._finish(this._currentActiveSpan.id);
+    // }
+    inject(span: Span, format = BaggageFormat.TEXT_MAP): object {
         let baggage = {};
-        if (this._currentActiveSpan)
-            this._tracer.inject(this._currentActiveSpan.context(), format, baggage);
+        this._tracer.inject(span.context(), format, baggage);
         return baggage;
     }
     extract(baggage: any, format: any): SpanContext | null {
