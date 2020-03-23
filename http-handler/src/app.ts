1764a6a6fc68ece3fcb77dcbdf3cb4c7c364c27f
dinzone <dinamitdana1@gmail.com>
2020-03-23 11:11:23 +0200

    try to struct the api

--- http-handler/src/app.ts
+++ http-handler/src/app.ts
@@ -8,7 +8,7 @@ import TracerRS, { spanRefType } from '../../utils/tracer';
 import { wait } from '../../utils';
 
 import { default as getRabbitInstance } from '../../utils/rabbitmq';
-import { Tags, Span, FORMAT_TEXT_MAP } from 'opentracing';
+import { Tags, Span, FORMAT_TEXT_MAP, REFERENCE_CHILD_OF } from 'opentracing';
 
 import { default as config } from '../../utils/config';
 
@@ -73,11 +73,51 @@ app.get('/trace', async (req, res) => {
     }
 });
 
+app.get('/tracePlus', async (req, res) => {
+    const tracer = new TracerRS(); // create tracer
+    tracer.startSpan('name'); // start root span of request
+    tracer.log('hello'); // when start root span the tracer reference to it
+    tracer.tag('hi', 'bye');
+    // create to child spans of the root span
+    const spanRef1 = tracer.startSpan('a', tracer.activeSpan(), REFERENCE_CHILD_OF),
+        spanRef2 = tracer.startSpan('b', tracer.activeSpan(), REFERENCE_CHILD_OF);
+    // bind the two span to funcion
+    // after the functions finish the spans finish too
+    await Promise.all([
+        tracer.withSpan(spanRef1, someIO, 3000),
+        tracer.withSpan(spanRef2, someIO, 2000)
+    ]);
+    tracer.log('bye'); // still reference to root span
+    const spanRef3 = tracer.startSpan('c', tracer.currentSpan(), REFERENCE_CHILD_OF); // create new child span
+    tracer.activeSpan(spanRef3); // change the active span, now every method that will call in tracer will reference to 'spanRef3'
+    tracer.tag('type', 'c');
+    tracer.log('change span');
+    wait(2000);
+    tracer.activeSpan(tracer.rootSpan()); // change back the active span to root span
+    tracer.log('finish request');
+    tracer.finishAll(); // finish all span tree
+    res.send(true);
+});
+
 start();
 
 async function start() {
     await rabbit.connect();
     app.listen(3000, () => {
         console.log('server running on port 3000');
+    });
+}
+
+function someIO(this: any, num: number): Promise<void> {
+    let self = this;
+    self.activeSpan().log('start');
+    self.activeSpan().tag({
+        ms: num
+    });
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            self.activeSpan().log('end');
+            res();
+        }, num);
     });
 }
